c3c5b2faf96f21d873d3e2be99d02d66b2089671
HeikkiHei <heikki.a.ahonen@helsinki.fi>
2016-01-28 20:49:53 +0200

    Update

--- MonsterGenerator/src/main/java/monster/logic/SystemLogic.java
+++ MonsterGenerator/src/main/java/monster/logic/SystemLogic.java
@@ -18,7 +18,6 @@ public class SystemLogic {
                 System.out.println("Valitse taso: 1-10");
                 inputInteger = Integer.parseInt(reader.nextLine());
             } catch (Exception e) {
-                System.out.println("Et antanut oikeaa syötettä. Anna kokonaisluku väliltä 1-10");
             }
             if (inputInteger <= 10 && inputInteger >= 1) {
                 System.out.println("Valitsit tason " + inputInteger);
@@ -35,12 +34,12 @@ public class SystemLogic {
                 System.out.println("Valitse tyyppi: Hirviö (H) tai NPC (N)");
                 inputType = reader.nextLine();
             } catch (Exception e) {
-                System.out.println("Et antanut oikeaa syötettä. Kirjoita H tai N");
             }
             if (inputType.equalsIgnoreCase("H")) {
                 System.out.println("Valitsit hirviön");
                 break;
-            } else if (inputType.equalsIgnoreCase("N")) {
+            }
+            if (inputType.equalsIgnoreCase("N")) {
                 System.out.println("Valitsit NPC:n");
                 break;
             }
